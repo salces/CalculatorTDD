04bde66bd6e17c5148888d0459a0019de5c4d27c
Piotr Wronski <piotr.wronski94@gmail.com>
2016-08-04 11:10:38 +0200

    Streams added

--- src/calc/StringCalculator.java
+++ src/calc/StringCalculator.java
@@ -1,8 +1,9 @@
 package calc;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class StringCalculator {
 
@@ -18,60 +19,55 @@ public class StringCalculator {
     }
 
     private int sumMultiNumbers(String numbers, String delimiters) throws NegativesNotAllowedException {
-
         String[] separatedNumbers = numbers.split(delimiters);
 
         checkForNegativeValues(separatedNumbers);
 
-        int sum = 0;
-        for (String s : separatedNumbers) {
-            int intFromString = Integer.parseInt(s);
-
-            if (intFromString <= 1000)
-                sum += intFromString;
-        }
-
-        return sum;
+        return  Arrays.stream(separatedNumbers)
+                .map(Integer::parseInt)
+                .filter(number -> number <= 1000)
+                .mapToInt(number -> number).sum();
     }
 
     private void checkForNegativeValues(String[] numbers) throws NegativesNotAllowedException {
-        List<String> negatives = new ArrayList<>();
-        for (String number : numbers) {
-            if (number.startsWith("-"))
-                negatives.add(number);
-        }
+        List<String> negatives;
+        negatives = Arrays.stream(numbers)
+                .filter(number -> number.startsWith("-"))
+                .collect(Collectors.toList());
 
-        if (!negatives.isEmpty())
+        if (!negatives.isEmpty()) {
             throw new NegativesNotAllowedException(negatives);
+        }
     }
 
     private String getDelimitersFrom(String expression) {
         String delimiters = ",|\n";
-        if (!expression.startsWith("//"))
+
+        if (!expression.startsWith("//")) {
             return delimiters;
+        }
 
         String delimitersBody = expression.split("\n", 2)[0];
         String newDelimiter = delimitersBody.substring(2);
-//        Pattern validDelimiterPattern = Pattern.compile("^//([.*])|(\\S)");
-//        Matcher matcher = validDelimiterPattern.matcher(delimitersBody);
-
 
         delimiters += "|" + Pattern.quote(newDelimiter);
-
 
         return delimiters;
     }
 
     private String getBodyOf(String expression) {
         String body = expression;
-        if (expression.startsWith("//"))
+
+        if (expression.startsWith("//")) {
             body = expression.split("\n", 2)[1];
+        }
+
         return body;
     }
 
     public static class NegativesNotAllowedException extends Exception {
 
-        String message;
+        private String message;
 
         public NegativesNotAllowedException(List<String> negatives) {
             message = getMessage(negatives);
